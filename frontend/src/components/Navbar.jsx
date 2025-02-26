e6d945103eb03f44baef3de4792336c12619f0a3
Ming Li <mingrkli@gmail.com>
2025-02-25 23:00:32 -0800

    Added the AddProduct Mockup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,21 @@
-import { Link } from 'react-router';
+import { Link } from "react-router";
 
 const NavBar = () => {
-  return (
-    <header>
-      <div>
-        <Link to="/">
-          <h1>Dashboard</h1>
-        </Link>
-        <Link to="/SpecimensExplorer">
-          <h1>SpecimensExplorer</h1>
-        </Link>
-      </div>
-    </header>
-  );
-}
+    return (
+        <header>
+            <div>
+                <Link to="/">
+                    <h1>Dashboard</h1>
+                </Link>
+                <Link to="/SpecimensExplorer">
+                    <h1>SpecimensExplorer</h1>
+                </Link>
+                <Link to="/AddProduct">
+                    <h1>AddProduct</h1>
+                </Link>
+            </div>
+        </header>
+    );
+};
 
 export default NavBar;
