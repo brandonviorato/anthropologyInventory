21cb8be9b0d89cedd92da3c52c597a9497a5954b
mehaksaini <saini.mehak@student.greenriver.edu>
2025-03-01 20:53:53 -0800

    updated specimensexplorer with sidebar added search bar and cards

--- frontend/src/pages/SpecimensExplorer.jsx
+++ frontend/src/pages/SpecimensExplorer.jsx
@@ -1,36 +1,95 @@
-//import { useEffect, useState} from 'react';
+import 'react';
+import { Link } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import Grid from '@mui/material/Grid';
 import SearchBar from '../components/SearchBar';
+import skull from "../images/test-skull.jpeg";
+import hand from "../images/test-hand.jpg"
+import '../index.css'; 
+export default function View() {
+ 
+  const specimens = [
+    {
+      id: "1", 
+      image: skull,
+      scientificName: "Skull",
+      description: "A human skull found in a cave.",
+    },
+    {
+      id: "2",
+      image: hand,
+      scientificName: "Example 2",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    },
+    {
+      id: "3",
+      image: skull,
+      scientificName: "Example 3",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    },
+    {
+      id: "4",
+      image: skull,
+      scientificName: "Example 4",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    },
+    {
+      id: "5",
+      image: hand,
+      scientificName: "Example 5",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    },
+    {
+      id: "6",
+      image: skull,
+      scientificName: "Example 6",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    }
+  ];
 
-const SpecimensExplorer = () => {
-    // const [specimens, setSpecimens] = useState(null);
+  return (
+    
+    <Grid container spacing={9.5}>
+      <Grid item xs={2.5} className="sidebar">
+        <Typography variant="h6">Search</Typography>
+        <Typography variant="body2">
+          <SearchBar className="searchBar_view"></SearchBar>
+        </Typography>
+      </Grid>
 
-    // const API_PORT = import.meta.env.VITE_API_PORT;
-    // const API_BASE_URL = `http://localhost:${API_PORT}`;
 
-    // useEffect(() => {
-    //     const fetchSpecimens = async () => {
-    //         const response = await fetch(`${API_BASE_URL}/api/specimens`);
-    //         const json = await response.json();
-
-    //         if (response.ok) {
-    //             setSpecimens(json);
-    //         }
-    //     }
-
-    //     fetchSpecimens()
-    // }, []);
-
-    return (
-        <div className="dashboard">
-            <h1>All Specimens</h1>
-            <SearchBar/>
-            {/* <div className='collections'>
-                {specimens && specimens.map((specimen) => (
-                    <p key={specimen._id}>{specimen.commonName}</p>
-                ))}
-            </div> */}
-        </div>
-    )
-};
-
-export default SpecimensExplorer;
+      <Grid item xs={8}>
+        <Grid container spacing={0} className="card-container">
+          {specimens.map((specimen, index) => (
+            <Grid item xs={5} key={index}>
+              <Card className="card">
+                <CardActionArea>
+                  <Link to={`/specimen/${specimen.id}`} style={{ textDecoration: "none" }}>
+                    <CardMedia
+                      component="img"
+                      className="card-media"
+                      image={specimen.image}
+                      alt={specimen.scientificName}
+                    />
+                    <CardContent className="card-content">
+                      <Typography className="card-title" variant="h5">
+                        {specimen.scientificName}
+                      </Typography>
+                      <Typography className="card-description" variant="body2">
+                        {specimen.description}
+                      </Typography>
+                    </CardContent>
+                  </Link>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
