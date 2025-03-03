e2d7d55461856e6ac77e2c3b98ed26bcd89f6310
mehaksaini <saini.mehak@student.greenriver.edu>
2025-03-02 20:46:14 -0800

    Added new imgs

--- frontend/src/pages/SpecimensExplorer.jsx
+++ frontend/src/pages/SpecimensExplorer.jsx
@@ -9,21 +9,23 @@ import Grid from '@mui/material/Grid';
 import SearchBar from '../components/SearchBar';
 import skull from "../images/test-skull.jpeg";
 import hand from "../images/test-hand.jpg"
+import child from "../images/taungChild.png";
+import lucy from "../images/lucy-skull.png";
 import '../index.css'; 
 export default function View() {
  
   const specimens = [
     {
       id: "1", 
-      image: skull,
-      scientificName: "Skull",
-      description: "A human skull found in a cave.",
+      image: lucy,
+      scientificName: "Lucy",
+      description: "The fossilized skull of Lucy, one of the most famous early human ancestors, discovered in Ethiopia in 1974. It provided crucial evidence of bipedalism in early hominins",
     },
     {
       id: "2",
-      image: hand,
-      scientificName: "Example 2",
-      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+      image: child,
+      scientificName: " Taung Child",
+      description: "The Taung Child is the first early hominin fossil discovered in Africa, providing key insights into human evolution. It is a well-preserved juvenile skull.",
     },
     {
       id: "3",
@@ -33,7 +35,7 @@ export default function View() {
     },
     {
       id: "4",
-      image: skull,
+      image: hand,
       scientificName: "Example 4",
       description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
     },
@@ -65,7 +67,7 @@ export default function View() {
       <Grid item xs={8}>
         <Grid container spacing={0} className="card-container">
           {specimens.map((specimen, index) => (
-            <Grid item xs={5} key={index}>
+            <Grid item xs={6} key={index}>
               <Card className="card">
                 <CardActionArea>
                   <Link to={`/specimen/${specimen.id}`} style={{ textDecoration: "none" }}>
